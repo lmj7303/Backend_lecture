60aa642c950ac560e656152c1030ffed3d94410b
Myungjae Lee <lmj7303@gmail.com>
2022-09-20 13:30:20 +0900

    nodejs

---
+++ 01.NodeJS/11.callee.js
@@ -0,0 +1,7 @@
+// 호출 당하는 프로그램(라이브러리)
+
+exports.randInt = function(from, to) {
+    return Math.floor(Math.random() * (to-from+1) + from);
+}
+
+exports.circleArea = radius => Math.PI * radius * radius;
